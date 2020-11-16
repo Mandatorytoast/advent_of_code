77c9bce15fb41773d139caa9b289ef0cdebd75a7
Zoey <optionaltoast420@gmail.com>
2020-11-16 19:35:54 +0000

    finished day1

---
+++ 2019/day1/solution.py
@@ -0,0 +1,27 @@
+from math import floor 
+def main():
+    with open('input.txt', 'r') as my_file:
+        file_list = tuple(map(int, my_file.read().strip().split('\n')))
+
+    part1 = [calculate_fuel_amount(x) for x in file_list]
+    part2 = [recursive_fuel_amount(x) for x in file_list]
+
+    print("Part one: " + str(sum(part1)))
+    print("Part two: " + str(sum(part2)))
+        
+
+def recursive_fuel_amount(mass): 
+    fuel = calculate_fuel_amount(mass)
+    if calculate_fuel_amount(fuel) <= 0:
+        return fuel
+    else:
+        return fuel + recursive_fuel_amount(fuel);
+
+
+def calculate_fuel_amount(mass):
+    new_mass = floor(mass/3) - 2
+    return new_mass
+
+if __name__ == "__main__":
+    main()
+    
